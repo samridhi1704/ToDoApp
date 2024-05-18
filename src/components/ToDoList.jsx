01286f744fc710a32478dc6c6c8d724fad52652c
sam <samridhi@hdiw.jdo>
2024-05-19 01:38:14 +0530

    CheckPoint3b

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,10 +2,24 @@ import ToDoItem from "./ToDoItem";
 
 export default function ToDoList()
 {
-    return <div className = "TaskList" >
-        <ul className="List" type='square'>
-            <ToDoItem /> 
-        </ul>
-    </div>
+
+    const items = [
+        "Read SpringBoot",
+        "Complete assignments",
+        "Prepare breakfast",
+        "Sleep for 2 hours",
+        "Take a shower"
+      ];
+
+      return (
+        <div className="TaskList">
+          <ul className="List" type='square'>
+            {items.map((item, index) => (
+              <ToDoItem key={index} text={item} />
+            ))}
+          </ul>
+        </div>
+      );
+    
 }
 
