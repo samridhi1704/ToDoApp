c636331b5a05e407470e9b6bc031ac584c215260
sam <samridhi@hdiw.jdo>
2024-05-20 00:19:39 +0530

    CheckPoint5a

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,3 +1,6 @@
+import React , {useState } from 'react';
+
+
 export default function ToDoItem({text})
 {
     /* const items = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"];
@@ -17,7 +20,14 @@ export default function ToDoItem({text})
      ))}
  </> */
 
+ const [completed , setCompleted] = useState(false);
+ const toggleCompleted = () => {
+     setCompleted(!completed); //toggle the completed state
+ }
 
 
- return <li> {text} </li>;
+ return (
+ <li style ={{textDecoration : completed ? 'line-through': 'none'}} onClick={toggleCompleted}>
+      {text} </li>
+ );
           }
